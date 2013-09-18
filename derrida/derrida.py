9f653d02c1ecda9a4c55b23b1e0a1ce8fa46f5f2
Nader Heidari <nukleas2001@gmail.com>
2013-09-18 00:11:09 -0400

    Starting up the project again
    Going to try to transfer as little code as possible from the legacy
    version, after all, I wrote that when I was first learning how to code.

---
+++ derrida/derrida.py
@@ -0,0 +1,24 @@
+# Derrida
+#
+# A deconstructionist program.
+#
+# By Nader Heidari
+#
+#
+# The purpose of this program is to act as a sort
+# of "build system" for text articles. A majority
+# of text in an article is not programmatically
+# relevant, so what we want to do is extract stuff
+# such as URLs, company names, DOIs, whatever, and
+# present them in a clean form.
+#
+# I chose the name derrida since the program ought
+# to deconstruct the articles with a touch of context,
+# provided by the programmer/reader.
+#
+# Pretentious? Sure, but let's get to the code.
+#
+
+
+class Extractor(object):
+    pass
